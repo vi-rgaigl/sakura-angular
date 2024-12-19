da074360d20d3cffd4dddf8cb7744c498594dd76
Regina Gaigl <regina.gaigl@gmail.com>
2024-12-19 10:41:34 +0100

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MainContentComponent } from './main-content/main-content.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component : MainContentComponent},
+    {path: 'imprint', component : ImprintComponent}
+];
