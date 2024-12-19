4f9bf2bcd47d6bd774423522c1e351e960ebde0b
Regina Gaigl <regina.gaigl@gmail.com>
2024-12-19 08:54:48 +0100

    html and scss main content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainContentComponent],
+  imports: [
+    RouterOutlet, 
+    MainContentComponent, 
+    FooterComponent,
+    ImprintComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
